cae31ad16ff8d9aee4fe3ef99bf91c176736a217
Chris <kuch517@gmail.com>
2021-06-06 21:18:18 -0700

    Added Postgres connection

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require("express");
+const { Client } = require('pg');
 
 const PORT = process.env.PORT || 3001;
 
@@ -7,6 +8,25 @@ const app = express();
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// Postgres connection
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//   if (err) throw err;
+//   for (let row of res.rows) {
+//     console.log(JSON.stringify(row));
+//   }
+//   client.end();
+// });
+
 
 app.get('/api', (req, res) => {
   res.json({ messsage: "Hello from server"});
